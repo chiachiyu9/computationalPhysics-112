72c1345987dab5751ce0a46c8ac148deb0628c04
Jocelyn <Jocelyn0929@gapp.nthu.edu.tw>
2024-06-14 14:07:32 +0800

    st

--- project2/nbody/particles.py
+++ project2/nbody/particles.py
@@ -5,8 +5,156 @@ import matplotlib.pyplot as plt
 class Particles:
     """
     Particle class to store particle properties
+    N: number of particles
+    r,v,a are position, velocity, acceleration in 3D.
     """
-    def __init__(self, N):
-        self.nparticles = N
+
+    def __init__(self, N):  # initial state when use this class
+        self.N = N
+        self._time = 0.0
+        self._m = np.ones((N, 1), dtype=np.float16)
+        self._r = np.zeros((N, 3))
+        self._v = np.zeros((N, 3))
+        self._a = np.zeros((N, 3))
+        self._tags = np.arange(1, N + 1)
         return
 
+    @property
+    def time(self):
+        return self._time
+
+    @time.setter
+    def time(self, t0: float):
+        self._time = t0
+        return
+
+    @property
+    def m(self):
+        return self._m
+
+    @m.setter
+    def m(self, m0: np.ndarray):
+        if m0.shape != (self.N, 1):  # (N,1)
+            raise ValueError("Shape of masses must be (N,1)")
+        self._m = m0
+        return
+
+    @property
+    def r(self):
+        return self._r
+
+    @r.setter
+    def r(self, r0: np.ndarray):
+        if r0.shape != (self.N, 3):
+            raise ValueError("Shape of positions must be (N,3)")
+        self._r = r0
+        return
+
+    @property
+    def v(self):
+        return self._v
+
+    @v.setter
+    def v(self, v0: np.ndarray):
+        if v0.shape != (self.N, 3):
+            raise ValueError("Shape of velocities must be (N,3)")
+        self._v = v0
+        return
+
+    @property
+    def a(self):
+        return self._a
+
+    @a.setter
+    def a(self, a0: np.ndarray):
+        if a0.shape != (self.N, 3):
+            raise ValueError("Shape of accelerations must be (N,3)")
+        self._a = a0
+        return
+
+    @property
+    def tags(self):
+        return self._tags
+
+    @tags.setter
+    def tags(self, tag: np.ndarray):
+        if tag.shape != np.arange(self.N).shape:
+            raise ValueError("Number of tags must be equal to N")
+        self._tags = tag
+        return
+
+    def add_particles(self, m, r, v, a):
+        """
+        Add N particles to the N-body simulation at once
+        """
+        self.N += m.shape[0]
+        self.masses = np.vstack((self.masses, m))
+        self.r = np.vstack((self.r, r))
+        self.v = np.vstack((self.v, v))
+        self.a = np.vstack((self.a, a))
+        self.tags = np.arange(self.N)
+        return
+
+    def set_particles(self, r, v, a):
+        """
+        Set particle properties for the N-body simulation
+        """
+        self.r = r
+        self.v = v
+        self.a = a
+        return
+
+    def output(self, filename):
+        """
+        Output a txt file with particles properties.
+        """
+        t = self.time
+        header = """
+                columns are tag, mass, x, y, z, vx, vy, vz, ax, ay, az
+                """
+        header += "Time=%f\n" % (t)
+        # N rows, ? columns.
+        # np.reshape(-1,n): -1 can altomatically calculate numbers of rows.
+        p = np.hstack(
+            (
+                self._tags.reshape(-1, 1),
+                self._m,
+                self._r,
+                self._v,
+                self._a,
+            )
+        )
+        np.savetxt(filename, p, delimiter=",", header=header, fmt="%.6f")
+        return
+
+    def draw(self, dim=2):
+        fig = plt.figure()
+        if dim == 2:
+            ax = fig.add_subplot(111)
+            ax.scatter(self.r[:, 0], self.r[:, 1])
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+        elif dim == 3:
+            ax = fig.add_subplot(111, projection="3d")
+            ax.scatter(self.r[:, 0], self.r[:, 1], self.r[:, 2])
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            ax.set_zlabel("z")
+        else:
+            print("Check dimension.")
+            return
+
+        ax.set_aspect("equal")
+        plt.tight_layout()
+        plt.show()
+        return fig, ax
+
+
+"""
+if __name__ == "__main__":
+    p = Particles(3)
+    tags = p.tags
+    print(tags)
+    # test error, should raise ValueError
+    # p.tags = []
+"""
--- project2/nbody/simulator.py
+++ project2/nbody/simulator.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
@@ -11,115 +12,254 @@ The N-Body Simulator class is responsible for simulating the motion of N bodies
 
 """
 
+
 class NBodySimulator:
 
     def __init__(self, particles: Particles):
-        
-        # TODO
+        self.particles = particles
+        self.time = particles.time
+        self.setup()
+        return
 
+    def setup(
+        self,
+        G=1,
+        rsoft=0.01,
+        method="RK4",
+        io_freq=10,
+        io_header="nbody",
+        io_screen=True,
+        visualization=False,
+    ):
+        """
+        The simulation enviroments.
+
+        G: graivtational constant
+        rsoft: float, a soften length
+        method: euler, RK2, RK4, kick(kick-drift-kick)
+        io_freq: int, the frequency to outupt data.
+                        io_freq <=0 for no output.
+        io_header: the output header
+        io_screen: print message on screen or not.
+        visualization: on the fly visualization or not.
+        """
+        self.G = G
+        self.rsoft = rsoft
+        self.method = method
+        self.io_header = io_header
+        self.io_screen = io_screen
+        self.visualization = visualization
+        if io_freq <= 0:
+            io_freq = np.inf
+        self.io_freq = io_freq
+        return
+
+    def evolve(self, dt: float, tf: float, ti=0.0):
+        """
+        evolving
+
+        dt: the time step
+        ti: initial time
+        tf: ending
+
+        """
+
+        t = ti
+        self.particles.time = ti
+        nt = int(np.ceil((tf - ti) / dt)) + 1  # 向上取整
+        particles = self.particles
+        particles.a = self._calculate_a(particles.N, particles.m, particles.r)
+
+        for n in range(nt):
+            if tf < t + dt:
+                dt = tf - t
+                # last step or wrong dt
+
+            # check IO
+            if (n % self.io_freq) == 0:
+                self.particles.time = t
+                # print info to screen
+                if self.io_screen:
+                    print("Time: ", t, "dt: ", dt)
+
+                # check output directroy
+                folder = "data_" + self.io_header
+                Path(folder).mkdir(parents=True, exist_ok=True)
+
+                # zfill(width): fill string to the width with 0 in front of it.
+                fn = folder + "/" + self.io_header + "_" + str(n).zfill(6) + ".dat"
+                self.particles.output(fn)
+
+                # visualization
+                if self.visualization:
+                    particles.draw()
+
+            particles = self._advance_particles(dt, particles)
+
+            t += dt
 
         return
 
-    def setup(self, G=1,
-                    rsoft=0.01,
-                    method="RK4",
-                    io_freq=10,
-                    io_header="nbody",
-                    io_screen=True,
-                    visualization=False):
-        """
-        Customize the simulation enviroments.
+    def _advance_particles(self, dt, particles):
 
-        :param G: the graivtational constant
-        :param rsoft: float, a soften length
-        :param meothd: string, the numerical scheme
-                       support "Euler", "RK2", and "RK4"
+        method = self.method
+        if method == "euler":
+            particles = self._advance_particles_Euler(dt, particles)
+        elif method == "RK2":
+            particles = self._advance_particles_RK2(dt, particles)
+        elif method == "RK4":
+            particles = self._advance_particles_RK4(dt, particles)
+        elif method == "kick":
+            particles = self._advance_particles_kick(dt, particles)
+        else:
+            print("method must be euler, RK2, RK4, kick")
+            raise (ValueError)
 
-        :param io_freq: int, the frequency to outupt data.
-                        io_freq <=0 for no output. 
-        :param io_header: the output header
-        :param io_screen: print message on screen or not.
-        :param visualization: on the fly visualization or not. 
-        """
-        
-        # TODO
+        return particles
 
-
-        return
-
-    def evolve(self, dt:float, tmax:float):
-        """
-        Start to evolve the system
-
-        :param dt: float, the time step
-        :param tmax: float, the total time to evolve
-        
-        """
-
-        # TODO
-
-
-
-
-
-
-
-
-
-        print("Simulation is done!")
-        return
-
-    def _calculate_acceleration(self, nparticles, masses, positions):
-        """
-        Calculate the acceleration of the particles
-        """
-        accelerations = np.zeros_like(positions)
-        
-
-        # TODO
-
-
-
-
-        return accelerations
-        
     def _advance_particles_Euler(self, dt, particles):
 
-        #TODO
+        N = particles.N
+        ms = particles.m
+        # last moment data
+        rs = particles.r
+        vs = particles.v
+        accs = self._calculate_a(N, ms, rs)
 
-
-
-
-
-
+        # record this moment
+        rs += vs * dt
+        vs += accs * dt
+        accs = self._calculate_a(N, ms, rs)
+        particles.set_particles(rs, vs, accs)
 
         return particles
 
     def _advance_particles_RK2(self, dt, particles):
 
-        # TODO
+        N = particles.N
+        ms = particles.m
+        rs = particles.r
+        # k1
+        vs = particles.v
+        accs = self._calculate_a(N, ms, rs)
 
+        # k2
+        rs2 = rs + vs * dt
+        vs2 = vs + accs * dt
+        accs2 = self._calculate_a(N, ms, rs2)
 
+        rsn = rs + 0.5 * dt * (vs + vs2)
+        vsn = vs + 0.5 * dt * (accs + accs2)
+        accsn = self._calculate_a(N, ms, rsn)
 
-
+        particles.set_particles(rsn, vsn, accsn)
 
         return particles
 
     def _advance_particles_RK4(self, dt, particles):
-        
-        #TODO
 
+        N = particles.N
+        ms = particles.m
 
+        # k1
+        rs = particles.r
+        vs = particles.v
+        accs = self._calculate_a(N, ms, rs)
+        # k2
+        rs2 = rs + 0.5 * dt * vs
+        vs2 = vs + 0.5 * dt * accs
+        accs2 = self._calculate_a(N, ms, rs2)
+        # k3
+        rs3 = rs + 0.5 * dt * vs2
+        vs3 = vs + 0.5 * dt * accs2
+        accs3 = self._calculate_a(N, ms, rs3)
+        # k4
+        rs4 = rs + dt * vs3
+        vs4 = vs + dt * accs3
+        accs4 = self._calculate_a(N, ms, rs4)
 
-
-
-
-
+        rsn = rs + dt / 6 * (vs + 2 * vs2 + 2 * vs3 + vs4)
+        vsn = vs + dt / 6 * (accs + 2 * accs2 + 2 * accs3 + accs4)
+        accsn = self._calculate_a(N, ms, rsn)
+        particles.set_particles(rsn, vsn, accsn)
 
         return particles
 
+    def _advance_particles_kick(self, dt, particles):
+
+        N = particles.N
+        ms = particles.m
+        rs = particles.r
+        vs = particles.v
+        accs = self._calculate_a(N, ms, rs)
+
+        vs_half = vs + 0.5 * dt * accs
+        rsn = rs + dt * vs_half
+        accsn = self._calculate_a(N, ms, rsn)
+        vsn = vs_half + 0.5 * dt * accsn
+        particles.set_particles(rsn, vsn, accsn)
+
+        return particles
+
+    def _calculate_a(self, N, m, x):
+        """
+        Calculate the acceleration of the particles
+        """
+        G = self.G
+        rsoft = self.rsoft
+
+        # invoke the kernel for acceleration calculation
+        accs = _calculate_a_kernel(N, m, x, rsoft, G)
+
+        return accs
+
+
+@njit(parallel=True)
+def _calculate_a_kernel(N, m, rr, rsoft, G):
+    """
+    When use numba, simpler data is, faster progrom runs.
+    Use more variables with smaller dim than less variables with larger dim.
+    """
+    x = rr[:, 0].copy()
+    y = rr[:, 1].copy()
+    z = rr[:, 2].copy()
+    mass = m[:, 0].copy()
+
+    acc = np.empty((N, 3), rr.dtype)
+
+    for i in prange(N):
+        ax = 0
+        ay = 0
+        az = 0
+        for j in prange(N):
+            if j != i:
+                dx = x[j] - x[i]
+                dy = y[j] - y[i]
+                dz = z[j] - z[i]
+
+                tmp = dx**2 + dy**2 + dz**2 + rsoft**2
+                factor = mass[j] / (tmp * np.sqrt(tmp))
+
+                ax += dx * factor
+                ay += dy * factor
+                az += dz * factor
+
+        acc[i, 0] = G * ax
+        acc[i, 1] = G * ay
+        acc[i, 2] = G * az
+
+    """
+    Fij = (
+        -G
+        * np.outer(m[:, 0], m[:, 0])[:, :, np.newaxis]
+        * rij
+        / r[:, :, np.newaxis] ** 3
+    )
+    acc = np.sum(Fij, axis=1) / m"""
+
+    return acc
 
 
 if __name__ == "__main__":
-    
+
     pass
